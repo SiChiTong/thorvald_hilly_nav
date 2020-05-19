5ba57ebe84ec0e070a58b1a451228ce3b780ebbd
vigneshrajap <vignesh.raja199@gmail.com>
2020-05-19 18:57:37 +0200

    online segmentation

--- scripts/online_segmentation.py
+++ scripts/online_segmentation.py
@@ -2,7 +2,7 @@
 import rospy
 import numpy as np
 import cv2
-from cv_bridge import CvBridge
+from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 import os
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide" # Hides the pygame version, welcome msg
@@ -24,6 +24,21 @@ class hilly_nav():
         self.crop_ratio = 0.3 # Ratio to crop the background parts in the image from top
 
         self.centerLine = []
+        self.bridge = CvBridge()
+
+        # subscribed Topic
+        self.subscriber = rospy.Subscriber("/kinect2_camera/rgb/image_color_rect",Image, self.callback)
+
+    def callback(self, ros_data):
+       #### direct conversion to CV2 ####
+       # np_arr = np.fromstring(ros_data.data, np.uint8)
+       # self.image = cv2.imdecode(np_arr, cv2.CV_LOAD_IMAGE_COLOR)
+       try:
+         self.image = self.bridge.imgmsg_to_cv2(ros_data, "bgr8")
+       except CvBridgeError as e:
+         print(e)
+
+       self.run_lane_fit()
 
     def segment_image(self):
 
@@ -36,7 +51,8 @@ class hilly_nav():
 
         coldata = np.sum(self.roi_img, axis=0)/255 # Sum the columns of warped image to determine peaks
 
-        self.modifiedCenters_local = signal.find_peaks(coldata, height=200, distance=self.roi_img.shape[1]/3)
+        self.modifiedCenters_local = signal.find_peaks(coldata, height=100, distance=self.roi_img.shape[1]/3)
+
 
         # print self.modifiedCenters_local[0][0]
 
@@ -53,10 +69,14 @@ class hilly_nav():
                cv2.circle(self.roi_img, (int(self.modifiedCenters_local[0][0]),int(self.roi_img.shape[0]-20)),
                                                                                 0, (255,0,255), thickness=30, lineType=8, shift=0)
 
+
        self.final_img = self.image
        # self.final_img = cv2.cvtColor(self.image, cv2.COLOR_GRAY2RGB)
        rheight, rwidth = self.final_img.shape[:2]
        self.final_img[int(rheight*self.crop_ratio):rheight,0:rwidth] = cv2.addWeighted( self.roi_img, 0.6,self.final_img[int(rheight*self.crop_ratio):int(rheight),0:rwidth], 0.8, 0)
+
+       cv2.imwrite('/home/saga/dummy.png', self.final_img) 
+
 
     def run_lane_fit(self):
        # Setting the parameters for upscaling and warping-unwarping
@@ -71,26 +91,13 @@ class hilly_nav():
        self.visualize_lane_fit(dst_size)
 
 if __name__ == '__main__':
+
+    # Initialize node
+    rospy.init_node('online_segmentation')
+    hilly_nav_obj = hilly_nav()
+
     try:
-        # Initialize node
-        rospy.init_node('lateral_heading_offset')
-
-        hilly_nav_obj = hilly_nav()
-        input_dir = expanduser("~/Hilly_Nav_Paper/plant_images/")
-        output_dir = expanduser("~/Hilly_Nav_Paper/fitted_images/")
-
-
-        for label_file in sorted(glob.glob(os.path.join(input_dir, '*.png'))):
-            print(label_file)
-            with open(label_file) as f:
-                base = os.path.splitext(os.path.basename(label_file))[0]
-                hilly_nav_obj.image = cv2.imread(label_file)
-
-                hilly_nav_obj.run_lane_fit()
-
-                # Store the Cropped Images
-                # cv2.imwrite(output_dir+base+'.png', hilly_nav_obj.final_img)
-
-    except rospy.ROSInterruptException:
-         cv2.destroyAllWindows() # Closes all the frames
-         pass
+        rospy.spin()
+    except KeyboardInterrupt:
+       print("Shutting down")
+    cv2.destroyAllWindows()
--- scripts/sliding_window_approach.py
+++ scripts/sliding_window_approach.py
@@ -475,7 +475,7 @@ class sliding_window():
        #cv2.circle(out_img, (modifiedCenters[1][1], modifiedCenters[1][0]), 8, (0, 255, 0), -1)
        midLane_i = []
        curves_m = []
-
+       print len(current_Pts)
        if len(current_Pts)>0:
         for c_in in range(len(current_Pts)): #
             cv2.polylines(out_img, [current_Pts[c_in]], 0, (0,255,255), thickness=5, lineType=8, shift=0)
